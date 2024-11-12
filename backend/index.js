5b463f8f326586140dfa3371abbf4a8f3cc418ba
Dhruv Gupta <kumardhruv05112002@gmail.com>
2024-11-12 22:38:06 +0530

    Completing Backend Logic

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.routes.js";
+import companyRoute from "./routes/company.routes.js";
+import connectDB from "./utils/db.js";
+
+dotenv.config({});
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin: 'http//localhost:5173',
+    credentials: true 
+}
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+// api's
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server Started at ${PORT}`);
+});
