67895a7fa14110b1c0785dd766ccbfa8d394ab66
Dhruv Gupta <kumardhruv05112002@gmail.com>
2024-11-15 17:12:25 +0530

    Added Job Application model and it's update

--- backend/models/application.model.js
+++ backend/models/application.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const applicationSchema = new mongoose.Schema({
     job: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Job',
+        ref: 'job',
         required: true
     },
     applicant: {
